4aeb1447cbf47af84f85210ed63858e28e089379
AdarshGoel2001 <86413540+AdarshGoel2001@users.noreply.github.com>
2022-12-04 01:19:40 +0530

    kuch bhi

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,46 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+
+import { Web3Modal } from "@web3modal/react";
+
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+
+const chains = [
+  chain.mainnet,
+  chain.polygon,
+  chain.goerli,
+  chain.polygonMumbai,
+];
+
+// Wagmi client
+const { provider } = configureChains(chains, [
+  walletConnectProvider({ projectId: "8bbf95b53425f9d0f7356b8d3ef09810" }),
+]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({ appName: "web3Modal", chains }),
+  provider,
+});
+
+// Web3Modal Ethereum Client
+const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <WagmiConfig client={wagmiClient}></WagmiConfig>
+
+      <Web3Modal
+        projectId="8bbf95b53425f9d0f7356b8d3ef09810"
+        ethereumClient={ethereumClient}
+      />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,17 @@ import Head from "next/head";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
 import deved from "../public/bank.gif";
-import logo from "../public/our-logo.png"
+import logo from "../public/our-logo.png";
 import kyc from "../public/kyc.webp";
 import borrower from "../public/borrower.webp";
 import lender from "../public/lender.webp";
 import Image from "next/image";
-
+import { useWeb3Modal, Web3Button } from "@web3modal/react";
+import { useAccount } from "wagmi";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
+  const { address, isConnected } = useAccount();
 
   return (
     <div className={darkMode ? "dark" : ""}>
@@ -22,7 +24,8 @@ export default function Home() {
       <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-20">
         <section className="min-h-screen">
           <nav className="py-2 mb-12 flex justify-between dark:text-white">
-          <Image src={logo} width={165} height={75} />
+            <Image src={logo} width={165} height={75} />
+
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill
@@ -30,14 +33,7 @@ export default function Home() {
                   className=" cursor-pointer text-2xl"
                 />
               </li>
-              <li>
-                <a
-                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="#"
-                >
-                  Form
-                </a>
-              </li>
+              <li></li>
             </ul>
           </nav>
           <div className="text-center p-10 py-10">
@@ -47,9 +43,10 @@ export default function Home() {
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
               Under Collateral & ZK Loans
             </h3>
-            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-              
-            </p>
+            {!isConnected && (
+              <button onClick={() => open()}>Connect Wallet</button>
+            )}
+
             <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
               <Image src={deved} layout="fill" objectFit="cover" />
             </div>
@@ -61,7 +58,8 @@ export default function Home() {
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Solving the biggest
               <span className="text-teal-500"> Banking </span>
-              Problem by providing <span className="text-teal-500"> Under Collateralized </span>
+              Problem by providing{" "}
+              <span className="text-teal-500"> Under Collateralized </span>
               Loans
             </p>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
@@ -71,28 +69,18 @@ export default function Home() {
           <div className="lg:flex gap-10">
             <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
               <Image src={borrower} width={300} height={200} />
-              <h3 className="text-lg font-medium pt-8 pb-2  ">
-                Borrower
-              </h3>
-              <p className="py-2">
-                Cds;kadsjbfasdkjbfsad;;vksdajb;dsv
-              </p>
+              <h3 className="text-lg font-medium pt-8 pb-2  ">Borrower</h3>
+              <p className="py-2">Cds;kadsjbfasdkjbfsad;;vksdajb;dsv</p>
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
               <Image src={lender} width={300} height={200} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">
-                Lender
-              </h3>
-              <p className="py-2">
-                waiefppfbiuwebfpuwebfpwuebfpuiew
-              </p>
+              <h3 className="text-lg font-medium pt-8 pb-2 ">Lender</h3>
+              <p className="py-2">waiefppfbiuwebfpuwebfpwuebfpuiew</p>
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
               <Image src={kyc} width={300} height={200} />
               <h3 className="text-lg font-medium pt-8 pb-2 ">KYC Verifier</h3>
-              <p className="py-2">
-                d;sandfnsaod;fnosa;dnfosdaknfo;nsd
-              </p>
+              <p className="py-2">d;sandfnsaod;fnosa;dnfosdaknfo;nsd</p>
             </div>
           </div>
         </section>
